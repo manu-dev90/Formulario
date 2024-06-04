93f3ff75ddb485b61d6f5f6c0eb7a32012418759
Manuel Maleras Sequera <manuel-antonio.maleras@getronics.com>
2024-06-04 16:49:19 +0200

    Feat branding color and scss changes

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -5,39 +5,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.scss'],
 })
 export class FormularioComponent {
-
-  validationInput(event: KeyboardEvent) 
-  {
+  validationInput(event: KeyboardEvent) {
     const input = event.key;
 
-    if(input === 'Backspace') 
-      {
-        return;
-      }
+    if (input === 'Backspace') {
+      return;
+    }
 
-    if(isNaN(Number(input)))
-      {
-        event.preventDefault();
-      }
+    if (isNaN(Number(input))) {
+      event.preventDefault();
+    }
   }
-  
-  
+
   fechaInicio: Date[] | null;
   fechaFin: Date[] | null;
   lista: any[];
-  nombre: string ;
-  cantidad: number | null ;
+  nombre: string;
+  cantidad: number | null;
   descripcion: string;
   minDate: Date;
   maxDate: Date;
   es: any;
 
-
-  constructor()
-  {
+  constructor() {
     this.fechaInicio = null;
     this.fechaFin = null;
     this.lista = [];
@@ -46,44 +39,39 @@ export class FormularioComponent {
     this.descripcion = '';
     this.minDate = new Date();
     this.maxDate = new Date();
-  
   }
 
-  setMinDate(event: any)
-  {
-    this.maxDate = event
+  setMinDate(event: any) {
+    this.maxDate = event;
   }
 
-  onSubmit() 
-  {
-
-    if(!this.verificarFormulario()) {
-      alert("Por favor, rellene todos los campos");
+  onSubmit() {
+    if (!this.verificarFormulario()) {
+      alert('Por favor, rellene todos los campos');
       return;
     }
 
-    this.lista.push(
-      {
-        nombre: this.nombre,
-        cantidad: this.cantidad,
-        descripcion: this.descripcion,
-        fechaInicio: this.fechaInicio,
-        fechaFin: this.fechaFin
-      }
-    );
+    this.lista.push({
+      nombre: this.nombre,
+      cantidad: this.cantidad,
+      descripcion: this.descripcion,
+      fechaInicio: this.fechaInicio,
+      fechaFin: this.fechaFin,
+    });
 
-    this.nombre = '' ;
+    this.nombre = '';
     this.cantidad = null;
     this.descripcion = '';
     this.fechaInicio = null;
-    this.fechaFin = null; 
-
+    this.fechaFin = null;
   }
-   verificarFormulario(): boolean {
-    return this.nombre.trim() !== '' &&
-            this.cantidad !== null &&
-              this.descripcion.trim() !== '' &&
-                this.fechaInicio !== null && this.fechaFin !== null;
-   }
-
+  verificarFormulario(): boolean {
+    return (
+      this.nombre.trim() !== '' &&
+      this.cantidad !== null &&
+      this.descripcion.trim() !== '' &&
+      this.fechaInicio !== null &&
+      this.fechaFin !== null
+    );
+  }
 }
