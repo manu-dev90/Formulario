5dd2c2651fa9d52917404edee3806056be2528fa
manu-dev90 <manu.linaressalas@gmail.com>
2024-06-14 19:46:48 +0200

    Solo componente Calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './formulario/formulario.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -11,14 +9,11 @@ import { __values } from 'tslib';
 import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
-
-import { PrimeNGConfig } from 'primeng/api';
 import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent,
     CalendarComponent,
   ],
   imports: [
@@ -36,19 +31,4 @@ import { CalendarComponent } from './calendar/calendar.component';
 })
 export class AppModule { 
 
-  constructor (private primengConfigEs: PrimeNGConfig) 
-  {
-    this.primengConfigEs.setTranslation(
-      {
-        dayNames: ["diumenge","dilluns","dimarts","dimecres","dijous","divendres","dissabte"],
-        dayNamesShort: ["diu","dil","dim","dix","dij","div","dis"],
-        dayNamesMin: ["diu.","dil.","dim.","dix.","dij.","div.","dis."],
-        monthNames: ["Gener","Febrer","Març","Abril","Maig","Juny","Juliol","Agost","Setembre","Octubre","Novembre","Desembre"],
-        monthNamesShort: ["Gen","Feb","Mar","Abr","Mai","Jun","Jul","Ago","Set","Oct","Nov","Des"],
-        today: 'Hoy',
-        clear: 'Limpiar',
-        firstDayOfWeek: 1,
-      }
-    );
-  }
 }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
+
 export class CalendarComponent {
 
   fechaInicio: Date | null;
@@ -18,8 +20,11 @@ export class CalendarComponent {
   maxDateInicio: Date;
   maxDateFin: Date;
   es: any;
+  ca: any;
+  translate: string;
+  buttonLabel: string;
 
-  constructor() {
+  constructor(private primengConfig: PrimeNGConfig) {
     this.fechaInicio = null;
     this.fechaFin = null;
     this.lista = [];
@@ -30,9 +35,53 @@ export class CalendarComponent {
     this.minDateFin = new Date(1900, 0, 1);
     this.maxDateInicio = new Date(3000, 11, 31);
     this.maxDateFin = new Date(3000, 11, 31);
+
+    this.ca =
+      {
+        dayNames: ["diumenge","dilluns","dimarts","dimecres","dijous","divendres","dissabte"],
+        dayNamesShort: ["diu","dil","dim","dix","dij","div","dis"],
+        dayNamesMin: ["diu","dil","dim","dix","dij","div","dis"],
+        monthNames: ["Gener","Febrer","Març","Abril","Maig","Juny","Juliol","Agost","Setembre","Octubre","Novembre","Desembre"],
+        monthNamesShort: ["Gen","Feb","Mar","Abr","Mai","Jun","Jul","Ago","Set","Oct","Nov","Des"],
+        today: 'Hoy',
+        clear: 'Limpiar',
+        firstDayOfWeek: 1,
+      };
+
+      this.es =
+        {
+          dayNames: ["domingo","lunes","martes","miércoles","jueves","viernes","sábado"],
+          dayNamesShort: ["dom","lun","mar","mié","jue","vie","sáb"],
+          dayNamesMin: ["dom","lun","mar","mié","jue","vie","sáb"],
+          monthNames: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septimbre","Octubre","Noviembre","Diciembre"],
+          monthNamesShort: ["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],
+          today: 'Hoy',
+          clear: 'Limpiar',
+          firstDayOfWeek: 1,
+        };
+
+        this.translate = 'ca';
+        this.buttonLabel = 'Castellano'
+        this.primengConfig.setTranslation(this.ca);
   }
 
-  setMinDateInicio(event: Date) {
+  botonTraducir()
+  {
+    if(this.translate === 'ca')
+      {
+        this.primengConfig.setTranslation(this.es);
+        this.translate = 'es';
+        this.buttonLabel = 'Castellano';
+      } else
+      {
+        this.primengConfig.setTranslation(this.ca);
+        this.translate = 'ca';
+        this.buttonLabel = 'Català';
+      }
+  }
+
+  setMinDateInicio(event: Date) 
+  {
     this.fechaInicio = event;
     this.minDateFin = event;
 
@@ -53,4 +102,67 @@ export class CalendarComponent {
       }
   }
 
+  restrictInputLength(event: KeyboardEvent, maxLength: number)
+  {
+    const input = event.target as HTMLInputElement;
+    if(input.value.length >= maxLength && !this.isControlKey(event))
+      {
+        event.preventDefault();
+      }
+  }
+
+  isControlKey(event: KeyboardEvent): boolean
+  {
+    const controlKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete'];
+    return controlKeys.includes(event.key);
+  }
+
+  validateInput(fieldName: string) 
+  {
+    const inputField = document.querySelector(`p-calendar[[(ngModel)]="${fieldName}"] input`) as HTMLInputElement;
+    const value = inputField.value;
+    const regex = /^\d{2}\/\d{2}\/\d{2} \d{2}:\d{2}$/;
+
+    if (regex.test(value)) 
+      {
+      const parts = value.split(/[\s/:]/);
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1; 
+      const year = parseInt(parts[2], 10) + 2000; 
+      const hours = parseInt(parts[3], 10);
+      const minutes = parseInt(parts[4], 10);
+      const date = new Date(year, month, day, hours, minutes);
+
+
+    if (fieldName === 'fechaInicio') {
+        this.fechaInicio = date;
+        this.minDateFin = date;
+
+    if (this.fechaFin && this.fechaFin < this.fechaInicio) 
+        {
+       this.fechaFin = null;
+        }
+      
+    } else if (fieldName === 'fechaFin') 
+      {
+      this.fechaFin = date; 
+      this.maxDateInicio = date;
+
+    if (this.fechaInicio && this.fechaInicio > this.fechaFin) 
+        {
+        this.fechaInicio = null;
+        }
+      }
+
+    } else {
+
+    if (fieldName === 'fechaInicio') 
+      {
+        this.fechaInicio = null;
+      } else if (fieldName === 'fechaFin') 
+        {
+        this.fechaFin = null;
+      }
+    }
+  }
 }
